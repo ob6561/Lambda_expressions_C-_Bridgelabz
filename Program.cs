b5bf9ad5cef4b116f9976c2ddfa02db91f9428f7
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-12-10 12:09:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lambda_expressions_C__Bridgelabz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
